2ecd345587dbf73d6bcd918ebca1cd6008fd2dee
pumpkin78 <pumpkin.arthur@gmail.com>
2014-11-23 12:24:47 -0800

    Updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,12 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCacheMatrix will create 4 functions associated with a matrix x:
+## set: use superassignment to cache value matrix x to an enclosing environment where x is defined
+## if not such an environment exists, then the global environment is used 
+## get: return matrix x
+## setinv: cache the inverse of x
+## getinv: return the inverse of x
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
@@ -16,7 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve return the inverse of matrix x, given x was created by makeCacheMatrix
+## It first check if the inverse matrix was cached, if yes, return it
+## If not, then compute the inverse, caches it, then return it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
